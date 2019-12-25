48e5f4944b16d54846bf44f176ef35579cf61fd2
Brian Ingles <github@emeraldwalk.com>
2019-12-24 21:59:46 -0600

    Commands working

--- packages/0.5x/src/components/App.tsx
+++ packages/0.5x/src/components/App.tsx
@@ -4,22 +4,98 @@ import { Editable, Slate, withReact } from 'slate-react'
 
 import { methods } from '../data/editor.json'
 
+const initialValue = [
+  {
+    type: 'div',
+    children: [
+      {
+        type: 'paragraph',
+        children: [
+          {
+            text: 'Path 000'
+          }
+        ],
+      },
+      {
+        type: 'paragraph',
+        children: [
+          {
+            text: 'Path 001'
+          }
+        ],
+      }
+    ]
+  },
+]
+
+function buildArg(
+  arg: (typeof methods)[0]['args'][0],
+  value: string,
+) {
+  if (arg.type === 'string') {
+    return value
+  }
+
+  let parsed
+  try {
+    parsed = JSON.parse(value)
+  }
+  catch (e) {
+    parsed = value
+  }
+
+  return value
+    ? parsed
+    : undefined
+}
+
+function isArgValid(
+  arg: (typeof methods)[0]['args'][0],
+  value: string,
+): boolean {
+  if (arg.type === 'string') {
+    return true
+  }
+
+  let parsed
+  try {
+    parsed = JSON.parse(value)
+  }
+  catch (e) {
+    parsed = value
+  }
+
+  return (arg.isOptional && value === '') || parsed
+}
+
+function runCommand(
+  name: string,
+  editor: Editor,
+  ...args: unknown[]
+) {
+  console.log('runCommand:', name, '[editor]', ...args)
+  return (Editor as any)[name](editor, ...args)
+}
+
 const App: React.FC = () => {
   const editor = useMemo(
     () => withReact(createEditor()),
     []
   )
 
-  const [value, setValue] = useState<Node[]>([
-    {
-      type: 'paragraph',
-      children: [
-        {
-          text: ''
-        }
-      ],
-    }
-  ])
+  const [value, setValue] = useState<Node[]>(initialValue)
+  const [methodState, setMethodState] = useState(() =>
+    methods.reduce(
+      (memo, method) => {
+        memo[method.name] = method.args.reduce((memo, arg) => {
+          memo[arg.name] = ''
+          return memo
+        }, {} as Record<string, string>)
+        return memo
+      },
+      {} as Record<string, Record<string, string>>
+    )
+  )
 
   return (
     <div className="c_app">
@@ -34,47 +110,75 @@ const App: React.FC = () => {
         />
       </Slate>
 
-      {
-        methods.map(({ args, name }) => (
-          <div className="c_method" key={name}>
-            <label className="c_method__label">
-              <span>Editor.{name}(</span>
-                {
-                  args.map(
-                    (arg, i) => (
-                      <div
-                        className="c_method__arg"
-                        key={arg.name}
-                        title={`${arg.name}: ${arg.type}`}
-                      >
-                        <span
-                          className="c_method__arg-label"
-                        >{arg.name}</span>
+      <div className="c_tools">
+        <div className="c_methods">
+          {
+            methods.map(({ args, name }) => (
+              <div className="c_method" key={name}>
+                <label className="c_method__label">
+                  <span>Editor.{name}(</span>
+                  {
+                    args.map(
+                      (arg, i) => (
+                        <div
+                          className="c_method__arg"
+                          key={arg.name}
+                          title={`${arg.name}: ${arg.type}`}
+                        >
+                          <span
+                            className="c_method__arg-label"
+                          >{arg.name}{arg.isOptional ? '?' : ''}</span>
 
-                        <input
-                          className="c_method__arg-input"
-                          onChange={({ currentTarget }) => {
-                            console.log(currentTarget.value)
-                          }}
-                          placeholder={arg.type}
-                          readOnly={i === 0}
-                          value={''}
-                        />
-                      </div>
+                          <input
+                            className="c_method__arg-input"
+                            onChange={({ currentTarget }) => {
+                              setMethodState({
+                                ...methodState,
+                                [name]: {
+                                  ...methodState[name],
+                                  [arg.name]: currentTarget.value,
+                                },
+                              })
+                            }}
+                            placeholder={arg.type}
+                            readOnly={i === 0}
+                            value={methodState[name][arg.name]}
+                          />
+                        </div>
+                      )
                     )
-                  )
-                }
-              <span>)</span>
-            </label>
+                  }
+                  <span>)</span>
+                </label>
 
-            <button onClick={() => {
-              const result = (Editor as any)[name](editor)
-              console.log('result:', JSON.stringify(result, undefined, 2))
-            }}>Go</button>
-          </div>
-        ))
-      }
+                <button
+                  disabled={args.slice(1).some(a => !isArgValid(a, methodState[name][a.name]))}
+                  onClick={() => {
+                    const result = runCommand(
+                      name,
+                      editor,
+                      ...args.slice(1).map(
+                        a => buildArg(
+                          a,
+                          methodState[name][a.name]
+                        )
+                      )
+                    )
+                    console.log('result:', JSON.stringify(result, undefined, 2))
+                  }}
+                >Go</button>
+              </div>
+            ))
+          }
+        </div>
 
+        <div className="c_value">
+          <h2>Value</h2>
+          <pre>
+            {JSON.stringify(value, undefined, 2)}
+          </pre>
+        </div>
+      </div>
     </div>
   )
 }
